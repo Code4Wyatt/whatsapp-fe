a2e52d979cb0e8b5133236621674eadde7770837
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-02-24 13:12:43 +0100

    c

--- src/Components/SideBarHeader.jsx
+++ src/Components/SideBarHeader.jsx
@@ -1,4 +1,26 @@
+import {BsChatLeftTextFill, BsThreeDotsVertical} from "react-icons/bs"
+import {Container, Row, Col} from "react-bootstrap"
+
+//  here need to import user data from store
+
 const SideBarHeader = ()=>{
-     return ( <h1>SideBarHeader</h1>)
+     return (
+
+          <Container>
+               <Row style={{border:'1px solid gray'}}>
+                    <Col xs={9}>
+                    <img src="https://picsum.photos/50" alt="avatar" style={{borderRadius:"50%"}} />
+                    </Col>
+                    <Col xs={3}>
+                    <BsChatLeftTextFill style={{fontSize:"20px", color:"gray"}}/><BsThreeDotsVertical style={{fontSize:"20px", color:"rgb(84 101 11)"}}/>
+                    </Col>
+                    
+               </Row>
+          </Container>
+
+          
+          
+          
+     )
 }
      export default SideBarHeader
